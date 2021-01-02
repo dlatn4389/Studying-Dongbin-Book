22bf110bd44efbb5c9d72318367867a87f0a340d
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-02 20:01:26 +0900

    1이 될 때까지

---
+++ Problem3_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Problem3_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputInts = sc.nextLine();
+        String[] inputInt = inputInts.split(" ");
+        int N, K;
+        N = Integer.parseInt(inputInt[0]);
+        K = Integer.parseInt(inputInt[1]);
+
+        int answer = 0;
+        while (N!=1) {
+            if (N%K==0) {
+                N /= K;
+            }
+            else {
+                N -= 1;
+            }
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
