31672514bbedd1a6a5e6235c02eeb0a7800fb68e
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-10 17:08:38 +0900

    위에서 아래로

---
+++ Problem6_10.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem6_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] nums = new int[T];
+        for (int i=0; i<T; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+        for (int i=T-1; i>=0; i--) {
+            System.out.println(nums[i]);
+        }
+    }
+}
