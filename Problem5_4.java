2c47af71f12c148a83a1bf95563e5a067945627f
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2021-01-10 10:10:33 +0900

    재귀 함수 종료 예제

---
+++ Problem5_4.java
@@ -0,0 +1,14 @@
+public class Problem5_4 {
+    public static void main(String[] args) {
+        recursive_function(0);
+    }
+
+    public static void recursive_function(int i) {
+        if (i==100) {
+            return;
+        }
+        System.out.println(i + "번째 재귀 함수에서 " + (i+1) + "번째 재귀 함수를 호출합니다.");
+        recursive_function(i+1);
+        System.out.println(i + "번째 재귀 함수를 종료합니다.");
+    }
+}
